8847eda1d41f6c8f53984c813a3e0e4ff76131cc
Yevhen Vasylevytskyi <spirit.ua87@gmail.com>
2021-11-19 11:25:37 +0200

    refactoring in process

--- src/App.js
+++ src/App.js
@@ -1,27 +1,24 @@
 // import { useState, useEffect } from 'react';
 import Container from './components/Container/Container';
-// import Section from './components/Section/Section';
+import Section from './components/Section/Section';
 // import FormAddContact from './components/FormAddContact/FormAddContact';
 // import ContactsList from './components/ContactsList/ContactsList';
 // import Filter from './components/Fliter/Filter';
 // import HomePage from './pages/Home';
-import ContactsPage from './pages/Contacts';
-import AddContact from './pages/AddContact';
+import ContactsPage from './components/ContactsList/Contacts';
+import AddContact from './components/AddContact/AddContact';
 
 export default function App() {
   return (
     <Container>
-      {/* <Section title="Phonebook">
-        <FormAddContact />
+      <Section title="Phonebook">
+        <AddContact />
       </Section>
 
       <Section title="Contacts">
-        <Filter />
-        <ContactsList />
-      </Section> */}
+        <ContactsPage />
+      </Section>
       {/* <HomePage /> */}
-      <AddContact />
-      <ContactsPage />
     </Container>
   );
 }
---
+++ src/components/ContactsList/Contacts.js
@@ -0,0 +1,26 @@
+import { ContactList } from 'components/ContactsList/ContactsList';
+import {
+  useFetchContactsQuery,
+  useDeleteContactMutation,
+} from 'redux/contacts/contactsApi';
+
+const ContactsPage = () => {
+  const { data /*error, isFetching, isError*/ } = useFetchContactsQuery();
+  const [deleteContact, { isLoading: isDeleting }] = useDeleteContactMutation();
+
+  return (
+    <>
+      <div>
+        {data && (
+          <ContactList
+            contacts={data}
+            onDelete={deleteContact}
+            deleting={isDeleting}
+          />
+        )}
+      </div>
+    </>
+  );
+};
+
+export default ContactsPage;
